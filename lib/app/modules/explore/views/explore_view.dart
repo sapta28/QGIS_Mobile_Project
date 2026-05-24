df375f5bc277855251958f5fdd4c6808e306f6d3
Avendika <avendikawahyusaputra@gmail.com>
2026-05-24 23:41:28 +0700

    p

--- lib/app/modules/explore/views/explore_view.dart
+++ lib/app/modules/explore/views/explore_view.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ExploreView extends StatefulWidget {
   const ExploreView({super.key});
@@ -79,6 +81,7 @@ class _ExploreViewState extends State<ExploreView> {
           ),
         ),
       );
+    }
   }
 
 class _TopAppBar extends StatelessWidget {
