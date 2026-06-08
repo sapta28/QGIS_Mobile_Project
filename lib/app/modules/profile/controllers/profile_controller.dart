ca23fb8fdf729ef0c9321fe70b292ef704ec8549
Sapta Adzani Purnama <saptaadzanipurnama@gmail.com>
2026-06-09 00:22:53 +0700

    change logic for some fitur in profile page

--- lib/app/modules/profile/controllers/profile_controller.dart
+++ lib/app/modules/profile/controllers/profile_controller.dart
@@ -1,18 +1,22 @@
 import 'package:dio/dio.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 import '../../../data/services/api/auth_api_service.dart';
 import '../../../data/services/api/auth_token_store.dart';
 import '../../../routes/app_pages.dart';
+import '../../../../models/models.dart';
 
 class ProfileController extends GetxController {
 	ProfileController(this._authApiService, this._tokenStore);
 
 	final AuthApiService _authApiService;
 	final AuthTokenStore _tokenStore;
+	final _storage = GetStorage();
 
 	final name = ''.obs;
 	final email = ''.obs;
+	final phone = ''.obs;
 	final role = ''.obs;
 	final avatarUrl = ''.obs;
 	final companyId = ''.obs;
@@ -20,11 +24,48 @@ class ProfileController extends GetxController {
 	final isLoading = false.obs;
 	final isSaving = false.obs;
 	final errorMessage = ''.obs;
+	final isNotificationsEnabled = true.obs;
+	final savedBillboards = <BillboardModel>[].obs;
 
 	@override
 	void onInit() {
 		super.onInit();
+		isNotificationsEnabled.value = _storage.read('is_notifications_enabled') ?? true;
+		loadSavedBillboards();
 		fetchProfile();
+	}
+
+	void loadSavedBillboards() {
+		try {
+			final List<dynamic>? rawList = _storage.read<List<dynamic>>('saved_billboards');
+			if (rawList != null) {
+				savedBillboards.value = rawList
+						.map((item) => BillboardModel.fromJson(Map<String, dynamic>.from(item)))
+						.toList();
+			} else {
+				savedBillboards.clear();
+			}
+		} catch (e) {
+			savedBillboards.clear();
+		}
+	}
+
+	bool isBillboardSaved(String id) {
+		return savedBillboards.any((b) => b.id == id);
+	}
+
+	void toggleSaveBillboard(BillboardModel billboard) {
+		if (isBillboardSaved(billboard.id)) {
+			savedBillboards.removeWhere((b) => b.id == billboard.id);
+		} else {
+			savedBillboards.add(billboard);
+		}
+		_storage.write('saved_billboards', savedBillboards.map((b) => b.toJson()).toList());
+	}
+
+	void toggleNotifications(bool value) {
+		isNotificationsEnabled.value = value;
+		_storage.write('is_notifications_enabled', value);
 	}
 
 	Future<void> fetchProfile() async {
@@ -41,8 +82,9 @@ class ProfileController extends GetxController {
 				// userId.value = data['id']?.toString() ?? '';
 				name.value = data['name']?.toString() ?? '';
 				email.value = data['email']?.toString() ?? '';
+				phone.value = data['phone']?.toString() ?? _storage.read('local_phone') ?? '';
 				// role.value = data['role']?.toString() ?? '';
-				// avatarUrl.value = data['avatar_url']?.toString() ?? '';
+				avatarUrl.value = _storage.read('local_avatar_path') ?? data['avatar_url']?.toString() ?? '';
 				// companyId.value = data['company_id']?.toString() ?? '';
 			}
 		} catch (error) {
@@ -54,9 +96,15 @@ class ProfileController extends GetxController {
 		}
 	}
 
+	void updateLocalAvatar(String path) {
+		avatarUrl.value = path;
+		_storage.write('local_avatar_path', path);
+	}
+
 	Future<bool> updateProfile({
 		required String name,
 		required String email,
+		String? phone,
 	}) async {
 		if (isSaving.value) {
 			return false;
@@ -67,16 +115,27 @@ class ProfileController extends GetxController {
 			final response = await _authApiService.updateProfile(
 				name: name,
 				email: email,
+				phone: phone,
 			);
 			final data = response['data'] is Map ? response['data'] : response;
 			if (data is Map) {
 				this.name.value = data['name']?.toString() ?? this.name.value;
 				this.email.value = data['email']?.toString() ?? this.email.value;
+				if (phone != null) {
+					this.phone.value = data['phone']?.toString() ?? phone;
+				}
+				avatarUrl.value = _storage.read('local_avatar_path') ?? data['avatar_url']?.toString() ?? avatarUrl.value;
 				role.value = data['role']?.toString() ?? role.value;
-				avatarUrl.value =
-						data['avatar_url']?.toString() ?? avatarUrl.value;
-				companyId.value =
-						data['company_id']?.toString() ?? companyId.value;
+				companyId.value = data['company_id']?.toString() ?? companyId.value;
+			} else {
+				this.name.value = name;
+				this.email.value = email;
+				if (phone != null) {
+					this.phone.value = phone;
+				}
+			}
+			if (phone != null) {
+				_storage.write('local_phone', phone);
 			}
 			Get.snackbar('Profile', 'Profil berhasil diperbarui.');
 			return true;
